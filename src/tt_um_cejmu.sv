d04e5f9c667b0039a200ce3e759edecd62b9d232
Jonathan Hager <mail@hager-jonathan.de>
2024-08-31 14:18:32 +0200

    Added baverage project with small test case

--- src/tt_um_cejmu.sv
+++ src/tt_um_cejmu.sv
@@ -17,22 +17,32 @@ module tt_um_cejmu (
 );
 
     logic [7:0]        project_mux;
+    logic              rst;
+    logic              bav0_out;
 
     always_comb begin
-        case(uio_in) // Multiplexer for submodule outputs. Replace cases
-          8'b00000000: project_mux = ui_in + ui_in;
-          8'b00000001: project_mux = ui_in - ui_in;
+        case(uio_in[1:0]) // Multiplexer for submodule outputs
+          2'b00: project_mux = {7'b0, bav0_out};
+          // 2'b01: project_mux = ui_in + ui_in;
 
           default: project_mux = ui_in;
         endcase;
     end
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = project_mux;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 8'b00000000;
+    // All output pins must be assigned. If not used, assign to 0.
+    assign uo_out  = project_mux;
+    assign uio_out = 0;
+    assign uio_oe  = 8'b00000000;
+    assign rst = !rst_n;
 
-  // List all unused inputs to prevent warnings
-  logic _unused = &{ena, clk, rst_n, 1'b0};
+    // List all unused inputs to prevent warnings
+    logic _unused = &{ena, clk, rst_n, 1'b0};
+
+    baverage bav0 (
+        .x(ui_in[1:0]),
+        .clk(uio_in[7]),
+        .rst(rst),
+        .y(bav0_out)
+    );
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -18,23 +18,32 @@ async def test_project(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.uio_in.value = 0  # Selecting baverage
+    dut.rst_n.value = 0  # reset
+    dut.uio_in.value = int('0b10000000', base=0)
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    dut.uio_in.value = int('0b00000000', base=0)
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    # assert dut.uo_out.value == 50
+    dut._log.info("Test project baverage")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # 50 cent input
+    dut.ui_in.value = 1
+    dut.uio_in.value = int('0b10000000', base=0)
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = int('0b00000000', base=0)
+    await ClockCycles(dut.clk, 1)
+
+    # 1 euro input
+    dut.ui_in.value = 2
+    dut.uio_in.value = int('0b10000000', base=0)
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = int('0b00000000', base=0)
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = int('0b10000000', base=0)
+    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = int('0b00000000', base=0)
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 1
